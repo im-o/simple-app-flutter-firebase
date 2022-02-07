ae3b05d1f1ff02039358db2ed78912dc673326a1
Rivaldy <rivaldy@stimednp.ac.id>
2022-02-07 22:27:44 +0800

    :label: SDK setup and configuration (web)

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyAvpn2c7FH1bjwJI5I3EV-83oCd4nTB5AQ",
+      authDomain: "flutter-user-profile-aaef2.firebaseapp.com",
+      projectId: "flutter-user-profile-aaef2",
+      storageBucket: "flutter-user-profile-aaef2.appspot.com",
+      messagingSenderId: "36293820089",
+      appId: "1:36293820089:web:ae71586fcfaba2de1b2568",
+      measurementId: "G-7CGVEJ74G1",
+    ),
+  );
 
   runApp(
     const MaterialApp(
